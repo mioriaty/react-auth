9869bd5d34182b014a2ef22023693fae0cd48047
mioriaty <mioriaty@gmail.com>
2021-06-05 11:09:49 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { AppContent } from 'containers/AppContent';
+import { store, persistor } from './store/configureStore';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<div />} persistor={persistor}>
+        <AppContent />
+      </PersistGate>
+    </Provider>
+  );
+}
+
+export default App;
